301cd4990c7104ffd06157989abe183cfeec8ca7
Nighthawk8 <40323493+Nighthawk8@users.noreply.github.com>
2019-01-29 17:20:29 -0600

    Add files via upload

---
+++ common/defines/LOTE_defines.lua
@@ -0,0 +1,6 @@
+NDefines.NGame.START_DATE = "1380.7.5"
+NDefines.NGame.END_DATE = "1900.1.1"
+NDefines.NDiplomacy.IMPERIAL_REFORM_COST = 75
+NDefines.NCountry.FREE_IDEA_GROUP_COST  = 4
+NDefines.NCountry.PS_BUY_NATIVE_ADVANCEMENT = 800
+NDefines.NGraphics.COLOR_WASTELANDS = 1
