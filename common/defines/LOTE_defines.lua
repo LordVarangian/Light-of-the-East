c2681c5a6a9c81fe2212466a00fcbc174a306bde
Nighthawk8 <nighthawkdavey@gmail.com>
2019-06-26 01:24:06 -0500

    end date and assyria loc fixes

--- common/defines/LOTE_defines.lua
+++ common/defines/LOTE_defines.lua
@@ -1,5 +1,4 @@
 NDefines.NGame.START_DATE = "1380.7.5"
-NDefines.NGame.END_DATE = "1900.1.1"
 NDefines.NDiplomacy.IMPERIAL_REFORM_COST = 75
 NDefines.NCountry.FREE_IDEA_GROUP_COST  = 4
 NDefines.NCountry.PS_BUY_NATIVE_ADVANCEMENT = 800
